bc87e06925625e071b95373145f7dbf5e7afb620
Devulpally-Sindhu <sindhu09.dev@gmail.com>
2024-11-30 12:01:16 +0530

    Changes-on-Reactive-formarray

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormArray, FormControl, FormGroup } from '@angular/forms';
+import { first } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Reactive-forms';
+  studentForm=new FormGroup({
+    firstname:new FormControl(),
+    lastname:new FormControl(),
+    mobilenumbers:new FormArray([new FormControl()]),
+    locations:new FormArray([new FormControl()])
+  })
+
+  addStudent(){
+    console.log(this.studentForm.value)
+  }
+
+  get mobilenumbers(){
+    return this.studentForm.get('mobilenumbers') as FormArray
+  }
+
+  get locations(){
+    return this.studentForm.get('locations') as FormArray
+  }
+
+  addNew(){
+    this.mobilenumbers.push(new FormControl())
+  }
+
+  addLocation(){
+    this.locations.push(new FormControl())
+  }
 }
