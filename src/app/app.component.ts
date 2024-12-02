947f0547aa6f607f0bbbf38361183d27ae4ee187
Devulpally-Sindhu <sindhu09.dev@gmail.com>
2024-12-02 17:36:56 +0530

    changes-on-check-box

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,28 +10,33 @@ import { first } from 'rxjs';
 export class AppComponent {
   title = 'Reactive-forms';
 
-  studentForm:any;
+  // studentForm:any;
+  student1:any[]=[];
 
-  constructor(public fb:FormBuilder){}
+  constructor(){}
   ngOnInit(){
-    this.studentForm=this.fb.group({
-      firstname:[''],
-      lastname:[''],
-      mobilenumbers:this.fb.array([[]])
-    })
+   
   }
 
-  get mobilenumbers(){
-    return this.studentForm.get('mobilenumbers') as FormArray
+  onCheck(e:any){
+    if(e.target.checked){
+      this.student1.push(e.target.value)
+    }
+    else{
+      this.student1=this.student1.filter((x)=>{
+        if(e.target.value!==x){
+          return true
+        }
+        else{
+          return false
+        }
+      })
+    }
+    
+   
   }
+  
 
-  add(){
-    console.log(this.studentForm.value)
-  }
-
-  addNew(){
-    this.mobilenumbers.push(this.fb.control(''))
-
-  }
+  
   
   }
