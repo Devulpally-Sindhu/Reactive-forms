bcbf480967a77a5bcf2650ac5bb01bbc6d5cde2e
Devulpally-Sindhu <sindhu09.dev@gmail.com>
2024-12-02 18:44:59 +0530

    df

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,12 +9,43 @@ import { first } from 'rxjs';
 })
 export class AppComponent {
   title = 'Reactive-forms';
+  studentForm:any;
 
-  gen:string=''
-
-  constructor(){}
+  constructor(public fb:FormBuilder){}
   ngOnInit(){
-   
+    this.studentForm=this.fb.group({
+      firstname:[''],
+      lastname:[''],
+      gender:[''],
+      techs:this.fb.array([''])
+    })  
+  }
+
+  add(){
+    console.log(this.studentForm.value)
+
+  }
+
+  get techs(){
+    return this.studentForm.get('techs') as FormArray
+  }
+
+  updateTechs(e:any){
+    // if(e.target.checked){
+    //   this.techs.push(e.target.value)
+    // }
+    // else{
+    //   this.techs=this.techs.filter((x:any)=>{
+    //     if(e.target.value!==x){
+    //       return true
+    //     }
+    //     else{
+    //       return false;
+    //     }
+    //   })
+    // }
+    this.techs.push(this.fb.control)
+
   }
 
   
